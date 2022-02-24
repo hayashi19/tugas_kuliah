6a8eea293472a353917ffe60887509975e465fac
Hayashi19 <55201076+hayashi19@users.noreply.github.com>
2022-02-24 14:38:06 +0700

    Update ClassDocter.java

--- semester2/pbo/tugas_offline_pbo/src/docter/ClassDocter.java
+++ semester2/pbo/tugas_offline_pbo/src/docter/ClassDocter.java
@@ -1,39 +1,40 @@
-package docter;
-
+package tugas_offline_bpo_2;
 import javax.swing.JOptionPane;
+import tugas_offline_bpo_2.*;
 
-public class ClassDocter {
+public class Tugas_offline_bpo_2 {
 
     public static void main(String[] args) {
         
         // Covert class to obj
-        ClassPerhitunganGaji objPerhitunganGaji = new ClassPerhitunganGaji();
+        MethodClass objPerhitunganGaji = new MethodClass();
         
         // Input buat data docternya
-        while(objPerhitunganGaji.docId.isEmpty()) {
-            objPerhitunganGaji.docId = JOptionPane.showInputDialog("Id Dokter:");
+        while(MethodClass.docId.isEmpty()) {
+            MethodClass.docId = JOptionPane.showInputDialog("Id Dokter:");
         }
         
-        while(objPerhitunganGaji.docNama.isEmpty()) {
-            objPerhitunganGaji.docNama = JOptionPane.showInputDialog("Nama Anda:");
+        while(MethodClass.docNama.isEmpty()) {
+            MethodClass.docNama = JOptionPane.showInputDialog("Nama Anda:");
         }
 
-        while(objPerhitunganGaji.docGaji.isEmpty()) {
-            objPerhitunganGaji.docGaji = JOptionPane.showInputDialog("Gaji:");
+        while(MethodClass.docGaji.isEmpty()) {
+            MethodClass.docGaji = JOptionPane.showInputDialog("Gaji:");
         }
 
         // buat variable pesan yang akan menampilkan semua data disampaikan pada message dialog
         String message =
                 "Data Dokter Anda" +
-                "\nId: " + objPerhitunganGaji.docId +
-                "\nNama: " + objPerhitunganGaji.docNama +
-                "\nGaji: " + objPerhitunganGaji.docGaji + 
-                "\nTunjangan anda: Rp " + objPerhitunganGaji.docTunjangan() + 
-                "\nTotal gaji anda: Rp " + objPerhitunganGaji.docTotalGaji();
+                "\nId: " + MethodClass.docId +
+                "\nNama: " + MethodClass.docNama +
+                "\nGaji: " + MethodClass.docGaji + 
+                "\nUang makan: Rp " + objPerhitunganGaji.uangMakan() + 
+                "\nTransportasi: Rp " + objPerhitunganGaji.transport() + 
+                "\nTotal gaji anda: Rp " + objPerhitunganGaji.totalGaji();
         
         // Tampilkan message dialog dengan value var [message]
         JOptionPane.showMessageDialog(null, message);
-    
+        
     }
     
 }
